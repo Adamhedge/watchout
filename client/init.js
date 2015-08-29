e01cd1c6e9dca3b5f70f977695e4fd2f55613ca6
adamhedge <adamhedge@gmail.com>
2015-08-28 21:29:49 -0700

    Implementing collision detection.

--- client/init.js
+++ client/init.js
@@ -1,4 +1,6 @@
-
+window.high = 0;
+window.current = 0;
+window.collision = 0;
 var makeEnemies = function() {
   var enemies = [];
   var boardWidth = d3.select('.gameboard').style('width');
@@ -23,7 +25,7 @@ var enemy = d3.select('.gameboard')
   .attr('y', function(d){return d[1]})
   .attr('class', 'enemy')
   .attr('xlink:href', 'http://freepngimages.com/wp-content/uploads/2015/06/jay-bird-flying-624x363.png')
-  .attr('height', 99)
+  .attr('height', 60)
   .attr('width', 100);
 
 var player = d3.select('.gameboard')
@@ -33,6 +35,7 @@ var player = d3.select('.gameboard')
   .attr('xlink:href', 'http://static.tumblr.com/69d05ead17652868f322dcaae9976507/99fnkes/7jsn0eevv/tumblr_static_rabbit1.png')
   .attr('height', 74)
   .attr('width', 75)
+  .attr('padding', 50)
   .attr('class', 'player');
 
 setInterval(function() {
@@ -44,3 +47,62 @@ setInterval(function() {
   }, 3000
 )
 
+d3.select("body").select("svg").on('mousemove', function() {
+  var pt = d3.mouse(this);
+  if(!wallcollision(pt)){
+    player.attr('x', pt[0]-45)
+      .attr('y', pt[1]-35);
+  }
+})
+
+
+var wallcollision = function(point) {
+  var boardx2 = window.innerWidth*0.8;
+  var boardy2 = 0.8 * window.innerHeight;
+
+  if(point[0]+25>boardx2||point[1]+25>boardy2||point[0]-40<0||point[1]-20<0) {
+    return true;
+  }
+  return false;
+}
+
+var high = 0;
+var current = 0;
+var collision = 0;
+var addToCollision = function(){
+  collision++;
+}
+
+setInterval(function(){
+  var colliding = false;
+  enemy[0].forEach(function(bird){
+    var playerx1 = +player.attr('x');
+    var playery1 = +player.attr('y')+10;
+    var playerx2 = playerx1 + 75;
+    var playery2 = playery1 + 55;
+    if(playerx2 < (+bird.getAttribute('x')) || (+bird.getAttribute('x')+100 )< playerx1 || playery2 < +bird.getAttribute('y') || (+bird.getAttribute('y')+60) < playery1){
+      current++;
+      console.log(colliding);
+      if(colliding === true){
+        colliding = false;
+      }
+      high = Math.max(high, current);
+      d3.selectAll('.numbers')
+        .data([high, current, collision])
+        .text(function(d){return d;});
+    } else{
+      high = Math.max(high, current);
+      console.log(colliding);
+      if(colliding === false){
+        collision++;  
+        colliding = true;
+      }
+      current = 0;
+      d3.selectAll('.numbers')
+        .data([high, current, collision])
+        .text(function(d){return d;});
+    }
+  });
+}, 10);
+
+//blood! path: 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z'
